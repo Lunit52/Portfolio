841891569b94b526a1a15e99bf3893852e395af5
Lunit52 <lucastodonnell@gmail.com>
2018-11-26 20:25:06 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+
+var link1 = document.getElementById("link1");
+
+
+
+
+link1.onmouseenter = blueBackground
+link1.onmouseleave = whiteBackground
+
+link2.onmouseenter = redBackground
+link2.onmouseleave = whiteBackground
+
+link3.onmouseenter = greenBackground
+link3.onmouseleave = whiteBackground
+
+// BLUE
+
+function blueBackground() {
+
+	document.body.style.backgroundColor = "#368FB2";
+}
+
+function whiteBackground() {
+
+	document.body.style.backgroundColor = "white";
+}
+
+
+//RED
+
+function redBackground() {
+
+	document.body.style.backgroundColor = "#FF8199";
+}
+
+function whiteBackground() {
+
+	document.body.style.backgroundColor = "white";
+}
+
+
+//GREEN
+
+function greenBackground() {
+
+	document.body.style.backgroundColor = "#67D4FF";
+}
+
+function whiteBackground() {
+
+	document.body.style.backgroundColor = "white";
+}
+
+// INFORMATION TRANSITION
+
+
+
